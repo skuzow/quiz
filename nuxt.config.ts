6e04803252411415f81560a69b82a3a41001c65c
skuzow <alex@skuzow.net>
2024-10-24 02:32:37 +0200

    feat: enable nuxt 4 app directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/eslint'],
+  future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  compatibilityDate: '2024-04-03'
 });
