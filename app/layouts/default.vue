bf2b9ab9c9d8e3ed7e9c833b2dd41e6f628bd6a9
skuzow <alex@skuzow.net>
2024-11-04 22:31:12 +0100

    feat: md margin-y default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="relative grid min-h-dvh grid-rows-[auto_1fr_auto] gap-2">
     <NavHeader />
-    <main class="container flex max-w-6xl flex-1 flex-col gap-10 py-4 md:px-8">
+    <main
+      class="container flex max-w-6xl flex-1 flex-col gap-10 py-4 md:my-11 md:px-8"
+    >
       <slot />
     </main>
     <NavFooter />
