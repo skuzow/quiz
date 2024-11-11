3741c26f903f2ff00a36c54c0d96054f1c3ad069
skuzow <alex@skuzow.net>
2024-11-11 23:35:41 +0100

    feat: seed prisma database
    roles, test categories, test question types & admin user

---
+++ app/constants/seed.ts
@@ -0,0 +1,17 @@
+export enum ROLE {
+  USER = 'User',
+  ADMIN = 'Admin'
+}
+
+export enum TEST_CATEGORY {
+  TECHNOLOGY = 'Technology',
+  UNIVERSITY = 'University',
+  SCIENCE = 'Science',
+  HISTORY = 'History',
+  LITERATURE = 'Literature'
+}
+
+export enum TEST_QUESTION_TYPE {
+  SINGLE = 'Single Response',
+  MULTIPLE = 'Multiple Response'
+}
---
+++ prisma/seed.ts
@@ -0,0 +1,101 @@
+import * as bcrypt from 'bcrypt';
+import { PrismaClient, type Role } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
+
+import { ROLE, TEST_CATEGORY, TEST_QUESTION_TYPE } from '@/constants/seed';
+
+const {
+  NUXT_TURSO_DATABASE_URL,
+  NUXT_TURSO_AUTH_TOKEN,
+
+  ADMIN_EMAIL,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD
+} = process.env;
+
+const libsql = createClient({
+  url: NUXT_TURSO_DATABASE_URL!,
+  authToken: NUXT_TURSO_AUTH_TOKEN!
+});
+
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter });
+
+const seedRoles = async () => {
+  const ROLES: string[] = Object.values(ROLE);
+
+  ROLES.forEach(async (role) => {
+    await prisma.role.upsert({
+      where: { name: role },
+      update: {},
+      create: { name: role }
+    });
+  });
+};
+
+const seedTestCategories = async () => {
+  const TEST_CATEGORIES: string[] = Object.values(TEST_CATEGORY);
+
+  TEST_CATEGORIES.forEach(async (testCategory) => {
+    await prisma.testCategory.upsert({
+      where: { name: testCategory },
+      update: {},
+      create: { name: testCategory }
+    });
+  });
+};
+
+const seedTestQuestionTypes = async () => {
+  const TEST_QUESTION_TYPES: string[] = Object.values(TEST_QUESTION_TYPE);
+
+  TEST_QUESTION_TYPES.forEach(async (testQuestionType) => {
+    await prisma.testQuestionType.upsert({
+      where: { name: testQuestionType },
+      update: {},
+      create: { name: testQuestionType }
+    });
+  });
+};
+
+const seedAdminUser = async () => {
+  const adminRole: Role | null = await prisma.role.findUnique({
+    where: { name: ROLE.ADMIN }
+  });
+
+  const userRole: Role | null = await prisma.role.findUnique({
+    where: { name: ROLE.USER }
+  });
+
+  await prisma.user.upsert({
+    where: { email: ADMIN_EMAIL! },
+    update: {},
+    create: {
+      email: ADMIN_EMAIL!,
+      username: ADMIN_USERNAME!,
+      password: await bcrypt.hash(ADMIN_PASSWORD!, 10),
+      emailVerified: true,
+      roles: {
+        create: [
+          { role: { connect: { id: adminRole!.id } } },
+          { role: { connect: { id: userRole!.id } } }
+        ]
+      }
+    }
+  });
+};
+
+const seed = async () => {
+  seedRoles();
+  seedTestCategories();
+  seedTestQuestionTypes();
+
+  seedAdminUser();
+};
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
