40a0815447c8d8c5e86c893af08d191f77671042
skuzow <alex@skuzow.net>
2024-11-25 04:03:38 +0100

    feat: more footer separation from content mobile

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative grid min-h-dvh grid-rows-[auto_1fr_auto] gap-2">
     <NavHeader />
-    <main class="container max-w-6xl py-4 md:my-11 md:px-8">
+    <main class="container mb-4 max-w-6xl py-4 md:my-11 md:px-8">
       <slot />
     </main>
     <NavFooter />
