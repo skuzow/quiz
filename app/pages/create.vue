5c85a8ae2ccf727b0ee085e610cebbff445ee253
skuzow <alex@skuzow.net>
2024-11-26 17:07:34 +0100

    feat: init about, create & create pages
    to avoid nuxt warnings

---
+++ app/pages/create.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+seoMeta();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-8xl">create</h1>
+  </div>
+</template>
---
+++ app/pages/createai.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+seoMeta();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-8xl">createai</h1>
+  </div>
+</template>
