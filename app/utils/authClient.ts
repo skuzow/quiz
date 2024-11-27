17eb7f83afe8b59350d8558e5b30c8fe6ff81526
skuzow <alex@skuzow.net>
2024-11-27 17:27:42 +0100

    fix: two factor client params
    due to better auth update

--- app/utils/authClient.ts
+++ app/utils/authClient.ts
@@ -2,12 +2,7 @@ import { createAuthClient } from 'better-auth/vue';
 import { usernameClient, twoFactorClient } from 'better-auth/client/plugins';
 
 const authClient = createAuthClient({
-  plugins: [
-    usernameClient(),
-    twoFactorClient({
-      twoFactorPage: '/two-factor'
-    })
-  ]
+  plugins: [usernameClient(), twoFactorClient()]
 });
 
 export default authClient;
