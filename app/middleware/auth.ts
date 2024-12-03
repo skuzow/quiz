9e810701f20783f82d10656a2c9330d0ceeb10ab
skuzow <alex@skuzow.net>
2024-12-03 04:43:18 +0100

    feat: lang error already logged in

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,11 +1,12 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
   const { isAuthenticated } = useAuth();
 
+  const nuxtApp = useNuxtApp();
   const localePath = useLocalePath();
 
   if (isAuthenticated.value) {
     if (to.path === localePath('/login') || to.path === localePath('/signup')) {
-      return abortNavigation('Already logged in');
+      return abortNavigation(nuxtApp.$i18n.t('error.alreadyLoggedIn'));
     }
   } else if (
     to.path === localePath('/create') ||
