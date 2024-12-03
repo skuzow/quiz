7bc20bfa3ab3f7c4392f8c7ef5fa96a41861b10b
skuzow <alex@skuzow.net>
2024-12-03 05:18:30 +0100

    feat: error page

---
+++ app/error.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import type { NuxtError } from '#app';
+
+interface Props {
+  error: NuxtError;
+}
+
+const { error } = defineProps<Props>();
+
+seoMeta({
+  title: String(error.statusCode),
+  description: error.message
+});
+
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <GradientSVG />
+
+  <NuxtLayout>
+    <section class="flex flex-col items-center gap-y-10">
+      <NavLogo :size="240" class="rotate-90 md:size-80" />
+
+      <div>
+        <h1 class="mb-1 flex justify-center text-4xl font-semibold">
+          ERROR {{ error.statusCode }}
+        </h1>
+
+        <p class="mb-10">
+          {{ error.message }}
+        </p>
+
+        <NuxtLink :to="localePath('/')">
+          <Button class="w-full">
+            {{ $t('nav.home') }}
+          </Button>
+        </NuxtLink>
+      </div>
+    </section>
+  </NuxtLayout>
+</template>
