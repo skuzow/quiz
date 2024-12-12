485138e3a05b1239dfd6746045506b439b699c4f
skuzow <alex@skuzow.net>
2024-12-12 15:41:38 +0100

    feat: tabs create ai

---
+++ app/components/create/ai/Tabs.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup></script>
+
+<template>
+  <Tabs default-value="file">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger value="file">{{ $t('createai.file.tab') }}</TabsTrigger>
+      <TabsTrigger value="text">{{ $t('createai.text.tab') }}</TabsTrigger>
+    </TabsList>
+    <TabsContent value="file">
+      <Card>
+        <CardHeader>
+          <CardTitle>{{ $t('createai.file.title') }}</CardTitle>
+          <CardDescription>
+            {{ $t('createai.file.description') }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div
+            class="flex h-40 w-full items-center justify-center rounded-md border border-dashed text-sm text-muted-foreground"
+          >
+            {{ $t('createai.file.input') }}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button class="w-full">{{ $t('createai.generate') }}</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+    <TabsContent value="text">
+      <Card>
+        <CardHeader>
+          <CardTitle>{{ $t('createai.text.title') }}</CardTitle>
+          <CardDescription>
+            {{ $t('createai.text.description') }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Textarea
+            class="h-40 w-full"
+            :placeholder="$t('createai.text.input')"
+          />
+        </CardContent>
+        <CardFooter>
+          <Button class="w-full">{{ $t('createai.generate') }}</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+  </Tabs>
+</template>
--- app/pages/createai.vue
+++ app/pages/createai.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 definePageMeta({ middleware: ['auth'] });
 
-seoMeta();
+const { t: $t } = useI18n();
+
+seoMeta({
+  title: $t('createai.title'),
+  description: $t('createai.description')
+});
 </script>
 
 <template>
-  <div>
-    <h1 class="text-8xl">createai</h1>
+  <div class="flex flex-col items-center">
+    <CommonTitle
+      :title="$t('createai.title')"
+      :description="$t('createai.description')"
+    />
+
+    <section class="flex w-80 flex-col gap-y-6 md:w-96">
+      <CreateAiTabs />
+    </section>
   </div>
 </template>
