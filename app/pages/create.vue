01bdf1fe2f08cc6bc27e31a721b8e77b3a1dc5c1
skuzow <alex@skuzow.net>
2024-12-22 11:48:13 +0100

    feat: temp created test display

--- app/pages/create.vue
+++ app/pages/create.vue
@@ -10,8 +10,36 @@ const createStore = useCreateStore();
   <div>
     <h1 class="text-8xl">create</h1>
 
-    <span v-if="createStore.createTestValue">
-      {{ createStore.createTestValue }}
-    </span>
+    <div v-if="createStore.createTestValue">
+      <h2>{{ createStore.createTestValue.title }}</h2>
+      <p class="mb-10">{{ createStore.createTestValue.description }}</p>
+
+      <ul>
+        <li
+          v-for="(question, index) in createStore.createTestValue.questions"
+          :key="index"
+          class="mb-6"
+        >
+          <h3 class="mb-2">
+            {{ index + 1 }}.
+            <span class="font-bold">{{ question.type }}</span> :
+            {{ question.text }}
+          </h3>
+
+          <ul>
+            <li v-for="(answer, index2) in question.options" :key="index2">
+              <p>
+                {{ answer.text }} :
+                <span
+                  :class="answer.isCorrect ? 'text-green-500' : 'text-red-500'"
+                >
+                  {{ answer.isCorrect }}
+                </span>
+              </p>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
