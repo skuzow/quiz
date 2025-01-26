d3dbcbc417f32a1f0e6566944a04a81c50dba346
skuzow <alex@skuzow.net>
2025-01-26 23:36:59 +0100

    feat: shared test & user constants

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,8 @@ import * as bcrypt from 'bcrypt';
 
 import prisma from '../server/utils/prisma';
 
-import { UserRole } from '@/constants/user';
-import { TestCategory, TestQuestionType } from '@/constants/test';
+import { UserRole } from '../shared/constants/user';
+import { TestCategory, TestQuestionType } from '../shared/constants/test';
 
 const { ADMIN_EMAIL, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
 
