a9098d3bd3a83a367002870fd41253c8fb92ce1d
skuzow <alex@skuzow.net>
2025-01-27 15:55:52 +0100

    feat: get test async data

--- app/stores/test.store.ts
+++ app/stores/test.store.ts
@@ -1,7 +1,18 @@
 export const useTestStore = defineStore('test', () => {
+  const { $api } = useNuxtApp();
+
   const createTest: Ref<IUserTest | undefined> = ref();
 
+  const getTest = async (id: string) => {
+    try {
+      return $api.test.get(id);
+    } catch (error) {
+      console.error('fetchPostList error:' + error);
+    }
+  };
+
   return {
-    createTest
+    createTest,
+    getTest
   };
 });
