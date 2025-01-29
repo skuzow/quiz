5a22d0d6b450c1679589009ca67843a381d394af
skuzow <alex@skuzow.net>
2025-01-29 14:19:02 +0100

    feat: landing top

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,46 @@
 <script lang="ts" setup>
+import { ArrowUpRightIcon, SparkleIcon } from 'lucide-vue-next';
+
 seoMeta();
 
-// const userStore = useUserStore();
-
-// const { status, data } = useAsyncData('getUser', () =>
-//   userStore.getUser('skuzow')
-// );
-
-// console.log('status', status.value);
-// console.log('data', data.value);
+const localePath = useLocalePath();
 </script>
 
 <template>
-  <div>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
-    <h1 class="text-8xl">{{ $t('nav.home') }}</h1>
+  <div class="mt-6 md:mt-10">
+    <section class="flex flex-col items-center text-center">
+      <h1 class="mb-5 text-5xl font-extrabold md:mb-8 md:max-w-3xl md:text-7xl">
+        <CommonGradientText>
+          {{ $t('landing.title.gradient') }}
+        </CommonGradientText>
+        {{ $t('landing.title.normal') }}
+      </h1>
+
+      <p class="mb-7 text-pretty md:mb-9 md:max-w-xl md:text-xl">
+        {{ $t('description') }}
+      </p>
+
+      <div class="flex gap-x-4">
+        <NuxtLink
+          :to="localePath('/tests')"
+          :title="$t('landing.buttons.start')"
+        >
+          <Button class="gap-x-2">
+            {{ $t('landing.buttons.start') }}
+            <ArrowUpRightIcon :size="16" />
+          </Button>
+        </NuxtLink>
+
+        <NuxtLink
+          :to="localePath('/createai')"
+          :title="$t('landing.buttons.ai')"
+        >
+          <Button variant="secondary" class="gap-x-2">
+            {{ $t('landing.buttons.ai') }}
+            <SparkleIcon :size="16" stroke="url(#gradient-svg)" />
+          </Button>
+        </NuxtLink>
+      </div>
+    </section>
   </div>
 </template>
