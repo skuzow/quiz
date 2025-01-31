7b504f48f1e318e94473c27d11b371434ba1bd4e
skuzow <alex@skuzow.net>
2025-01-31 14:19:38 +0100

    feat: init search tests feed

--- app/components/tests/Feed.vue
+++ app/components/tests/Feed.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ChevronDownIcon, SlidersHorizontalIcon } from 'lucide-vue-next';
+
 // interface Props {
 //   userId?: string;
 // }
@@ -11,18 +13,33 @@ const { status, data } = useAsyncData('tests', () => testStore.getTests(0, 14));
 </script>
 
 <template>
-  <p>{{ status }}</p>
-  <section class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-    <template v-if="status === 'pending'">
-      <TestsCardSkeleton v-for="index in 14" :key="index" />
-    </template>
+  <div class="flex flex-col gap-y-4">
+    <div class="flex gap-x-2">
+      <Input :placeholder="$t('tests.search.placeholder')" />
 
-    <template v-else>
-      <TestsCard
-        v-for="(test, index) in data?.body?.tests"
-        :key="index"
-        :test="test"
-      />
-    </template>
-  </section>
+      <Button class="gap-x-2" variant="outline">
+        {{ $t('tests.search.buttons.sort') }}
+        <ChevronDownIcon :size="16" />
+      </Button>
+
+      <Button class="gap-x-2" variant="secondary">
+        <SlidersHorizontalIcon :size="16" />
+        {{ $t('tests.search.buttons.filter') }}
+      </Button>
+    </div>
+
+    <section class="grid grid-cols-1 gap-4 lg:grid-cols-2">
+      <template v-if="status === 'pending'">
+        <TestsCardSkeleton v-for="index in 14" :key="index" />
+      </template>
+
+      <template v-else>
+        <TestsCard
+          v-for="(test, index) in data?.body?.tests"
+          :key="index"
+          :test="test"
+        />
+      </template>
+    </section>
+  </div>
 </template>
