6bd8a16beb061023736f9c27b1e8d95612be285b
skuzow <alex@skuzow.net>
2025-02-13 00:17:51 +0100

    fix: multiple option selected bg tests make correction

--- app/components/tests/make/correction/Multiple.vue
+++ app/components/tests/make/correction/Multiple.vue
@@ -9,11 +9,10 @@ const { selected } = defineProps<Props>();
 </script>
 
 <template>
-  <div class="h-4 w-4 shrink-0 rounded-sm border border-primary">
-    <CheckIcon
-      v-if="selected"
-      :size="14"
-      class="bg-primary stroke-background"
-    />
+  <div
+    class="h-4 w-4 shrink-0 rounded-sm border border-primary"
+    :class="{ 'bg-primary': selected }"
+  >
+    <CheckIcon v-if="selected" :size="14" class="stroke-background" />
   </div>
 </template>
