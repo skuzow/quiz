fe491597ce0afafb97621285a8595bc9ad9f7a8c
skuzow <alex@skuzow.net>
2025-02-13 12:02:17 +0100

    feat: user id & username tests feed

--- app/pages/users/id/[id].vue
+++ app/pages/users/id/[id].vue
@@ -2,17 +2,11 @@
 const route = useRoute();
 
 const userStore = useUserStore();
-const testStore = useTestStore();
 
 const id = route.params.id as string;
 
 const { status: userStatus, data: userData } = useAsyncData(`user-${id}`, () =>
   userStore.getUserById(id)
-);
-
-const { status: testsStatus, data: dataStatus } = useAsyncData(
-  `user-tests-${id}`,
-  () => testStore.getTestsById(id, 0, 14)
 );
 </script>
 
@@ -20,8 +14,7 @@ const { status: testsStatus, data: dataStatus } = useAsyncData(
   <div>
     <h1>{{ userStatus }}</h1>
     <p>{{ userData }}</p>
-    <br />
-    <h2>{{ testsStatus }}</h2>
-    <p>{{ dataStatus }}</p>
+
+    <TestsFeed :id="id" />
   </div>
 </template>
