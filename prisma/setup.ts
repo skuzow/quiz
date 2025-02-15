248a890aa5b0065d9e3f08cfcca1f1b6e285395d
skuzow <alex@skuzow.net>
2025-02-15 14:25:17 +0100

    feat: use local database in dev

--- prisma/setup.ts
+++ prisma/setup.ts
@@ -2,9 +2,15 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { createClient } from '@libsql/client';
 
-const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
+const { NODE_ENV, TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
 
-export const setupTursoDatabase = (): PrismaClient => {
+export const setupDatabase = (): PrismaClient => {
+  if (NODE_ENV === 'production') return setupTursoDatabase();
+
+  return setupLocalDatabase();
+};
+
+const setupTursoDatabase = (): PrismaClient => {
   const libsql = createClient({
     url: TURSO_DATABASE_URL as string,
     authToken: TURSO_AUTH_TOKEN as string
@@ -12,6 +18,12 @@ export const setupTursoDatabase = (): PrismaClient => {
 
   const adapter = new PrismaLibSQL(libsql);
   const prisma = new PrismaClient({ adapter });
+
+  return prisma;
+};
+
+const setupLocalDatabase = (): PrismaClient => {
+  const prisma = new PrismaClient();
 
   return prisma;
 };
