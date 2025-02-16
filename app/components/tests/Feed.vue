e7da9cda1dd03fcf375f6027586fb3db54449c64
skuzow <alex@skuzow.net>
2025-02-16 02:58:11 +0100

    feat: tests feed infinite scroll
    not cached data :(

--- app/components/tests/Feed.vue
+++ app/components/tests/Feed.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { ArrowDownUpIcon, FilterIcon } from 'lucide-vue-next';
 
+import { TESTS_PAGE_SIZE } from '#shared/constants/test';
+
 interface Props {
   id?: string;
   username?: string;
@@ -10,19 +12,58 @@ const { id, username } = defineProps<Props>();
 
 const testStore = useTestStore();
 
-const asyncDataKey = () => {
-  if (id) return `user-tests-${id}`;
-  if (username) return `user-tests-${username}`;
-  return 'tests';
+const tests: Ref<IUserTestPartial[]> = ref([]);
+
+const page: Ref<number> = ref(0);
+const isLoading: Ref<boolean> = ref(false);
+const hasMore: Ref<boolean> = ref(true);
+
+const testsRequest = async (): Promise<IUserTestPartial[] | undefined> => {
+  if (id) return testStore.getTestsById(id, page.value);
+  if (username) return testStore.getTestsByUsername(username, page.value);
+  return testStore.getTests(page.value);
 };
 
-const asyncDataFn = () => {
-  if (id) return testStore.getTestsById(id, 0);
-  if (username) return testStore.getTestsByUsername(username, 0);
-  return testStore.getTests(0);
+const loadTests = async () => {
+  isLoading.value = true;
+
+  try {
+    const response = await testsRequest();
+
+    const responseTests: IUserTestPartial[] = response.body.tests;
+
+    tests.value.push(...responseTests);
+
+    if (responseTests.length < TESTS_PAGE_SIZE) hasMore.value = false;
+    else page.value++;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (e) {
+    // 404
+    // TODO: handle 404 error with message (e.statusCode)
+    hasMore.value = false;
+  } finally {
+    isLoading.value = false;
+  }
 };
 
-const { status, data } = useAsyncData(asyncDataKey(), asyncDataFn);
+const infiniteScroll = useTemplateRef<HTMLElement>('infinite-scroll');
+
+const handleScroll = () => {
+  if (isLoading.value || !hasMore.value) return;
+
+  const infiniteScrollElement = infiniteScroll.value as HTMLElement;
+
+  if (infiniteScrollElement.getBoundingClientRect().bottom < window.innerHeight)
+    loadTests();
+};
+
+onMounted(() => {
+  loadTests();
+
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => window.removeEventListener('scroll', handleScroll));
 </script>
 
 <template>
@@ -42,18 +83,20 @@ const { status, data } = useAsyncData(asyncDataKey(), asyncDataFn);
     </div>
 
     <section>
-      <ol class="grid grid-cols-1 gap-4">
-        <template v-if="!data && status === 'pending'">
-          <li v-for="index in 14" :key="index">
+      <ol ref="infinite-scroll" class="grid grid-cols-1 gap-4">
+        <template v-if="tests.length">
+          <li v-for="(test, index) in tests" :key="index">
+            <TestsCard :test="test" />
+          </li>
+        </template>
+
+        <template v-if="!tests.length || isLoading">
+          <li v-for="index in TESTS_PAGE_SIZE" :key="index">
             <TestsCardSkeleton />
           </li>
         </template>
 
-        <template v-else>
-          <li v-for="(test, index) in data?.body?.tests" :key="index">
-            <TestsCard :test="test" />
-          </li>
-        </template>
+        <span v-if="!hasMore">No more tests :(</span>
       </ol>
     </section>
   </div>
