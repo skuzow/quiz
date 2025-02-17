245ae742bd590324d777b1178a2115857a05ed5a
skuzow <alex@skuzow.net>
2025-02-17 13:11:51 +0100

    feat: better auth middleware
    specific auth login & auth

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,17 +1,9 @@
-export default defineNuxtRouteMiddleware((to, _from) => {
+export default defineNuxtRouteMiddleware((_to, _from) => {
   const { isAuthenticated } = useAuth();
 
-  const nuxtApp = useNuxtApp();
-  const localePath = useLocalePath();
+  if (!isAuthenticated.value) {
+    const localePath = useLocalePath();
 
-  if (isAuthenticated.value) {
-    if (to.path === localePath('/login') || to.path === localePath('/signup')) {
-      return abortNavigation(nuxtApp.$i18n.t('error.alreadyLoggedIn'));
-    }
-  } else if (
-    to.path === localePath('/create') ||
-    to.path === localePath('/createai')
-  ) {
     return navigateTo(localePath('/login'));
   }
 });
