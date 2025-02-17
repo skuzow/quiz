1613f0ad1bf997f05abad11e7f8a353eb197c638
skuzow <alex@skuzow.net>
2025-02-17 12:59:57 +0100

    feat: edit test page init

---
+++ app/pages/tests/[id]/edit.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+definePageMeta({ middleware: ['auth'] });
+
+const { t: $t } = useI18n();
+
+seoMeta({
+  title: $t('edit.title'),
+  description: $t('edit.description')
+});
+
+const route = useRoute();
+
+const id = route.params.id as string;
+</script>
+
+<template>
+  <div>{{ id }}</div>
+</template>
