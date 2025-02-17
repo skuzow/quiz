55a454b39ad7e34a5f9f88d4e420fdfbd08d4c3d
skuzow <alex@skuzow.net>
2025-02-17 13:57:42 +0100

    feat: form & middleware tests edit

--- app/pages/tests/[id]/edit.vue
+++ app/pages/tests/[id]/edit.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-definePageMeta({ middleware: 'auth' });
+definePageMeta({ middleware: ['auth', 'tests-edit'] });
 
 const { t: $t } = useI18n();
 
@@ -7,24 +7,8 @@ seoMeta({
   title: $t('edit.title'),
   description: $t('edit.description')
 });
-
-const route = useRoute();
-
-const id = route.params.id as string;
-
-const testStore = useTestStore();
-
-const result = await testStore.getTestById(id);
-
-const { user } = useAuth();
-
-if (user.value?.id !== result.body.test.author.id) {
-  const localePath = useLocalePath();
-
-  await navigateTo(localePath(`/tests/${id}`));
-}
 </script>
 
 <template>
-  <div>{{ result }}</div>
+  <Create :edit="true" />
 </template>
