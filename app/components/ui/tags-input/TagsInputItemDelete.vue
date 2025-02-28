e71f085dba716d3086ce44d82501fc7e3b22b798
skuzow <alex@skuzow.net>
2025-02-28 19:53:20 +0100

    feat: command, tags input & dialog ui components

---
+++ app/components/ui/tags-input/TagsInputItemDelete.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { TagsInputItemDeleteProps } from 'radix-vue';
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+import { XIcon } from 'lucide-vue-next';
+import { TagsInputItemDelete, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+
+const props = defineProps<
+  TagsInputItemDeleteProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <TagsInputItemDelete
+    v-bind="forwardedProps"
+    :class="cn('mr-1 flex rounded bg-transparent', props.class)"
+  >
+    <slot>
+      <XIcon class="h-4 w-4" />
+    </slot>
+  </TagsInputItemDelete>
+</template>
