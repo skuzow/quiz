a6e604322811933bcbfea6112aad0599f5922162
skuzow <alex@skuzow.net>
2025-03-22 18:35:08 +0100

    feat(profile): skeleton user created at

--- app/components/users/profile/Skeleton.vue
+++ app/components/users/profile/Skeleton.vue
@@ -3,7 +3,11 @@
     <CommonTopSkeleton />
 
     <div class="flex flex-col gap-y-2">
-      <Skeleton class="h-[29.5px] w-5/6 rounded-md md:w-2/6" />
+      <div class="md:flex md:justify-between">
+        <Skeleton class="h-[29.5px] w-5/6 rounded-md md:w-2/6" />
+
+        <Skeleton class="hidden h-[27.5px] rounded-md md:flex md:w-1/6" />
+      </div>
 
       <Skeleton class="h-[21px] w-2/3 rounded-md md:w-1/6" />
 
@@ -12,6 +16,8 @@
           <Skeleton class="h-[22px] w-[60px] rounded-md" />
         </li>
       </ul>
+
+      <Skeleton class="h-[20px] w-2/6 rounded-md md:hidden" />
     </div>
   </div>
 </template>
