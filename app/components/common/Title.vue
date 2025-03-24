2febc1ae4c9e8248b7928588f14b99c173c32cc5
skuzow <alex@skuzow.net>
2025-03-24 16:47:06 +0100

    feat(common): title text center

--- app/components/common/Title.vue
+++ app/components/common/Title.vue
@@ -8,8 +8,8 @@ const { title, description } = defineProps<Props>();
 </script>
 
 <template>
-  <h1 class="mb-2 text-3xl font-bold">
+  <h1 class="mb-2 text-center text-3xl font-bold">
     <CommonGradientText>{{ title }}</CommonGradientText>
   </h1>
-  <p class="mb-10">{{ description }}</p>
+  <p class="mb-10 text-center">{{ description }}</p>
 </template>
