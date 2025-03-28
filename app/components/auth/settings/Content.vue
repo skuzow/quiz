36fe3beb806c1ba61feddb490354724daa8fa5a0
skuzow <alex@skuzow.net>
2025-03-28 02:36:13 +0100

    feat(settings): scroll when overflow y

--- app/components/auth/settings/Content.vue
+++ app/components/auth/settings/Content.vue
@@ -3,8 +3,8 @@ import { Settings2Icon } from 'lucide-vue-next';
 </script>
 
 <template>
-  <DialogContent>
-    <DialogHeader>
+  <DialogContent class="max-h-[90dvh] p-0">
+    <DialogHeader class="p-6 pb-0">
       <DialogTitle class="flex items-center gap-x-2.5">
         <Settings2Icon :size="20" />
         {{ $t('nav.header.user.settings') }}
@@ -13,7 +13,9 @@ import { Settings2Icon } from 'lucide-vue-next';
 
     <Separator />
 
-    <div class="flex flex-col gap-y-4 text-sm">
+    <div
+      class="flex max-h-[75dvh] flex-col gap-y-4 overflow-y-auto p-6 pt-0 text-sm"
+    >
       <AuthSettingsProfile />
 
       <AuthSettingsUsername />
