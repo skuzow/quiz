907f13369054227cf5cf22979bb2067e5a69c175
skuzow <alex@skuzow.net>
2025-03-28 15:36:49 +0100

    feat(settings): link account dropdown item

---
+++ app/components/auth/settings/account/linked/dropdown/index.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { LinkIcon } from 'lucide-vue-next';
+
+import { AuthProviderValues } from '@/constants/auth.constant';
+
+interface Props {
+  accountProviders?: string[];
+}
+
+const { accountProviders } = defineProps<Props>();
+
+const unlinkedProviders = computed(() =>
+  AuthProviderValues.filter((provider) => !accountProviders?.includes(provider))
+);
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button align="end" variant="ghost" class="gap-x-2">
+        <LinkIcon :size="16" />
+        {{ $t('auth.settings.account.linked.link') }}
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" class="w-44 font-medium">
+      <template
+        v-for="unlinkedProvider in unlinkedProviders"
+        :key="unlinkedProvider"
+      >
+        <AuthSettingsAccountLinkedDropdownItem :provider="unlinkedProvider" />
+      </template>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
