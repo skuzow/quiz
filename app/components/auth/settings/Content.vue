43be3f6558cf77a37b59cdb7cc5af3e069fdbe89
skuzow <alex@skuzow.net>
2025-03-30 14:30:52 +0200

    fix(settings): scroll bar occupies all content

--- app/components/auth/settings/Content.vue
+++ app/components/auth/settings/Content.vue
@@ -3,18 +3,16 @@ import { Settings2Icon } from 'lucide-vue-next';
 </script>
 
 <template>
-  <DialogContent class="max-h-[90dvh] p-0">
-    <DialogHeader class="p-6 pb-0">
+  <DialogContent class="max-h-[90dvh] gap-0 p-0">
+    <DialogHeader class="border-b p-6 pb-4">
       <DialogTitle class="flex items-center gap-x-2.5">
         <Settings2Icon :size="20" />
         {{ $t('nav.header.user.settings') }}
       </DialogTitle>
     </DialogHeader>
 
-    <Separator />
-
     <div
-      class="flex max-h-[75dvh] flex-col gap-y-4 overflow-y-auto p-6 pt-0 text-sm"
+      class="flex max-h-[75dvh] flex-col gap-y-4 overflow-y-auto p-6 pt-4 text-sm"
     >
       <AuthSettingsProfile />
 
