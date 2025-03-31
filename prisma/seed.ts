b3665f4ec29aa131ca7a3a2a9a5b815cedb6623a
skuzow <alex@skuzow.net>
2025-03-31 16:18:31 +0200

    chore(package): remove bcrypt dependency

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,3 @@
-import * as bcrypt from 'bcrypt';
-
 import prisma from '../server/utils/prisma';
 
 import { UserRole } from '../shared/constants/user.constant';
@@ -8,7 +6,7 @@ import {
   TestQuestionType
 } from '../shared/constants/test.constant';
 
-const { ADMIN_EMAIL, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
+// const { ADMIN_EMAIL, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
 
 const seedRoles = async () => {
   const roles: string[] = Object.values(UserRole);
@@ -46,72 +44,72 @@ const seedTestQuestionTypes = async () => {
   });
 };
 
-const seedAdminUser = async () => {
-  await prisma.user.upsert({
-    where: { email: ADMIN_EMAIL as string },
-    update: {},
-    create: {
-      email: ADMIN_EMAIL as string,
-      name: ADMIN_USERNAME as string,
-      username: ADMIN_USERNAME as string,
-      password: await bcrypt.hash(ADMIN_PASSWORD as string, 10),
-      emailVerified: true,
-      roles: {
-        create: [
-          { role: { connect: { name: UserRole.ADMIN } } },
-          { role: { connect: { name: UserRole.USER } } }
-        ]
-      },
-      tests: {
-        create: [
-          {
-            title: 'Example test title',
-            description: 'Example test description',
-            categories: {
-              create: [
-                { category: { connect: { name: TestCategory.TECHNOLOGY } } },
-                { category: { connect: { name: TestCategory.UNIVERSITY } } }
-              ]
-            },
-            questions: {
-              create: [
-                {
-                  number: 1,
-                  text: 'Single example question',
-                  type: { connect: { name: TestQuestionType.SINGLE } },
-                  options: {
-                    create: [
-                      { text: 'Single example option 1', isCorrect: true },
-                      { text: 'Single example option 2', isCorrect: false },
-                      { text: 'Single example option 3', isCorrect: false },
-                      { text: 'Single example option 4', isCorrect: false }
-                    ]
-                  }
-                },
-                {
-                  number: 2,
-                  text: 'Multiple example question',
-                  type: { connect: { name: TestQuestionType.MULTIPLE } },
-                  options: {
-                    create: [
-                      { text: 'Multiple example option 1', isCorrect: true },
-                      { text: 'Multiple example option 2', isCorrect: true },
-                      { text: 'Multiple example option 3', isCorrect: false },
-                      { text: 'Multiple example option 4', isCorrect: false }
-                    ]
-                  }
-                }
-              ]
-            },
-            completed: {
-              create: [{}, {}] // 2 completed
-            }
-          }
-        ]
-      }
-    }
-  });
-};
+// const seedAdminUser = async () => {
+//   await prisma.user.upsert({
+//     where: { email: ADMIN_EMAIL as string },
+//     update: {},
+//     create: {
+//       email: ADMIN_EMAIL as string,
+//       name: ADMIN_USERNAME as string,
+//       username: ADMIN_USERNAME as string,
+//       password: await bcrypt.hash(ADMIN_PASSWORD as string, 10),
+//       emailVerified: true,
+//       roles: {
+//         create: [
+//           { role: { connect: { name: UserRole.ADMIN } } },
+//           { role: { connect: { name: UserRole.USER } } }
+//         ]
+//       },
+//       tests: {
+//         create: [
+//           {
+//             title: 'Example test title',
+//             description: 'Example test description',
+//             categories: {
+//               create: [
+//                 { category: { connect: { name: TestCategory.TECHNOLOGY } } },
+//                 { category: { connect: { name: TestCategory.UNIVERSITY } } }
+//               ]
+//             },
+//             questions: {
+//               create: [
+//                 {
+//                   number: 1,
+//                   text: 'Single example question',
+//                   type: { connect: { name: TestQuestionType.SINGLE } },
+//                   options: {
+//                     create: [
+//                       { text: 'Single example option 1', isCorrect: true },
+//                       { text: 'Single example option 2', isCorrect: false },
+//                       { text: 'Single example option 3', isCorrect: false },
+//                       { text: 'Single example option 4', isCorrect: false }
+//                     ]
+//                   }
+//                 },
+//                 {
+//                   number: 2,
+//                   text: 'Multiple example question',
+//                   type: { connect: { name: TestQuestionType.MULTIPLE } },
+//                   options: {
+//                     create: [
+//                       { text: 'Multiple example option 1', isCorrect: true },
+//                       { text: 'Multiple example option 2', isCorrect: true },
+//                       { text: 'Multiple example option 3', isCorrect: false },
+//                       { text: 'Multiple example option 4', isCorrect: false }
+//                     ]
+//                   }
+//                 }
+//               ]
+//             },
+//             completed: {
+//               create: [{}, {}] // 2 completed
+//             }
+//           }
+//         ]
+//       }
+//     }
+//   });
+// };
 
 const seed = async () => {
   seedRoles();
