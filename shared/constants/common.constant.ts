cb4abc2e77c53eb0ac94e549c657feed3ec5a26c
skuzow <alex@skuzow.net>
2025-04-02 03:02:17 +0200

    feat(shared): common image size max constants

--- shared/constants/common.constant.ts
+++ shared/constants/common.constant.ts
@@ -1,2 +1,6 @@
 export const COMMON_TOP_IMAGE_HEIGHT: number = 172;
 export const COMMON_TOP_IMAGE_WIDTH: number = 1088;
+
+export const COMMON_IMAGE_SIZE_MAX_MB: number = 5;
+export const COMMON_IMAGE_SIZE_MAX: number =
+  COMMON_IMAGE_SIZE_MAX_MB * 1024 * 1024;
