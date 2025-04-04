f843b65c4e7b761c04f5565d3802d3f13dfdaf73
skuzow <alex@skuzow.net>
2025-04-04 15:11:06 +0200

    fix: disable i18n api translations
    due to i18n api cache error & prod not showing up api messages in error pages

--- i18n/localeDetector.ts
+++
@@ -1,9 +0,0 @@
-export default defineI18nLocaleDetector((event, config) => {
-  const cookie = tryCookieLocale(event, { lang: '', name: 'i18n_redirected' });
-  if (cookie) return cookie.toString();
-
-  const header = tryHeaderLocale(event, { lang: '' });
-  if (header) return header.toString();
-
-  return config.defaultLocale;
-});
