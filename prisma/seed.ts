bb5c420a0bf1d66087deadcadc33a0f14766099a
skuzow <alex@skuzow.net>
2025-04-04 16:54:10 +0200

    feat(prisma): delete unused seed admin user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,8 +5,6 @@ import {
   TestQuestionTypeValues
 } from '../shared/constants/test.constant';
 
-// const { ADMIN_EMAIL, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
-
 const seedTestCategories = async () => {
   TestCategoryValues.forEach(async (testCategory) => {
     await prisma.testCategory.upsert({
@@ -27,78 +25,9 @@ const seedTestQuestionTypes = async () => {
   });
 };
 
-// const seedAdminUser = async () => {
-//   await prisma.user.upsert({
-//     where: { email: ADMIN_EMAIL as string },
-//     update: {},
-//     create: {
-//       email: ADMIN_EMAIL as string,
-//       name: ADMIN_USERNAME as string,
-//       username: ADMIN_USERNAME as string,
-//       password: await bcrypt.hash(ADMIN_PASSWORD as string, 10),
-//       emailVerified: true,
-//       roles: {
-//         create: [
-//           { role: { connect: { name: UserRole.ADMIN } } },
-//           { role: { connect: { name: UserRole.USER } } }
-//         ]
-//       },
-//       tests: {
-//         create: [
-//           {
-//             title: 'Example test title',
-//             description: 'Example test description',
-//             categories: {
-//               create: [
-//                 { category: { connect: { name: TestCategory.TECHNOLOGY } } },
-//                 { category: { connect: { name: TestCategory.UNIVERSITY } } }
-//               ]
-//             },
-//             questions: {
-//               create: [
-//                 {
-//                   number: 1,
-//                   text: 'Single example question',
-//                   type: { connect: { name: TestQuestionType.SINGLE } },
-//                   options: {
-//                     create: [
-//                       { text: 'Single example option 1', isCorrect: true },
-//                       { text: 'Single example option 2', isCorrect: false },
-//                       { text: 'Single example option 3', isCorrect: false },
-//                       { text: 'Single example option 4', isCorrect: false }
-//                     ]
-//                   }
-//                 },
-//                 {
-//                   number: 2,
-//                   text: 'Multiple example question',
-//                   type: { connect: { name: TestQuestionType.MULTIPLE } },
-//                   options: {
-//                     create: [
-//                       { text: 'Multiple example option 1', isCorrect: true },
-//                       { text: 'Multiple example option 2', isCorrect: true },
-//                       { text: 'Multiple example option 3', isCorrect: false },
-//                       { text: 'Multiple example option 4', isCorrect: false }
-//                     ]
-//                   }
-//                 }
-//               ]
-//             },
-//             completed: {
-//               create: [{}, {}] // 2 completed
-//             }
-//           }
-//         ]
-//       }
-//     }
-//   });
-// };
-
 const seed = async () => {
   seedTestCategories();
   seedTestQuestionTypes();
-
-  // seedAdminUser();
 };
 
 seed()
