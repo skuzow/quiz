4f4a2576e6e150019f7ea0d99539c7ec47d8b705
skuzow <alex@skuzow.net>
2025-04-08 01:00:11 +0200

    test(e2e): i18n renders en & es translations

---
+++ app/test/e2e/i18n.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+import { setup, createPage, url } from '@nuxt/test-utils';
+import { mockNuxtImport } from '@nuxt/test-utils/runtime';
+
+mockNuxtImport('useI18n', () => () => useNuxtApp().$i18n);
+
+describe('i18n', async () => {
+  await setup();
+
+  const { t: $t, setLocale } = useI18n();
+
+  test('renders EN translation correctly in default route', async () => {
+    const page = await createPage();
+    await page.goto(url('/'));
+
+    const description = await page.textContent('p');
+
+    expect(description).toBe($t('description'));
+  });
+
+  test('renders ES translation correctly in /es route', async () => {
+    const page = await createPage();
+    await page.goto(url('/es'));
+
+    setLocale('es');
+    const description = await page.textContent('p');
+
+    expect(description).toBe($t('description'));
+  });
+});
