d01ca7f51a2f8b5e90f450b33e54bfaf91850baa
skuzow <alex@skuzow.net>
2025-04-07 16:34:17 +0200

    test(e2e): app can mount the website

---
+++ app/test/e2e/app.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from 'vitest';
+import { setup, createPage, url } from '@nuxt/test-utils';
+
+describe('app', async () => {
+  await setup();
+
+  test('can mount the website', async () => {
+    const page = await createPage();
+    await page.goto(url('/'), { waitUntil: 'hydration' });
+
+    const siteName = await page.textContent('span');
+    expect(siteName).toBe('skuzow/quiz');
+  });
+});
