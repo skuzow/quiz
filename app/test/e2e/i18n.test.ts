2e4d91cef4c47974713f616bf172fdd592fc5dd9
skuzow <alex@skuzow.net>
2025-04-28 01:25:50 +0200

    test(e2e): increase timeout i18n

--- app/test/e2e/i18n.test.ts
+++ app/test/e2e/i18n.test.ts
@@ -4,24 +4,32 @@ import { setup, createPage } from '@nuxt/test-utils/e2e';
 describe('i18n', async () => {
   await setup();
 
-  test('renders EN translation correctly in default route', async () => {
-    const page = await createPage('/');
+  test(
+    'renders EN translation correctly in default route',
+    { timeout: 10000 },
+    async () => {
+      const page = await createPage('/');
 
-    const description = await page.textContent('p');
+      const description = await page.textContent('p');
 
-    const { t: $t } = useNuxtApp().$i18n;
+      const { t: $t } = useNuxtApp().$i18n;
 
-    expect(description).toBe($t('description'));
-  });
+      expect(description).toBe($t('description'));
+    }
+  );
 
-  test('renders ES translation correctly in /es route', async () => {
-    const page = await createPage('/es');
+  test(
+    'renders ES translation correctly in /es route',
+    { timeout: 10000 },
+    async () => {
+      const page = await createPage('/es');
 
-    const description = await page.textContent('p');
+      const description = await page.textContent('p');
 
-    const { t: $t, setLocale } = useNuxtApp().$i18n;
-    await setLocale('es');
+      const { t: $t, setLocale } = useNuxtApp().$i18n;
+      await setLocale('es');
 
-    expect(description).toBe($t('description'));
-  });
+      expect(description).toBe($t('description'));
+    }
+  );
 });
