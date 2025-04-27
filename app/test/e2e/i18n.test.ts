b7b4ffdd4937d9e2bea36d85a50d994035f80881
skuzow <alex@skuzow.net>
2025-04-27 23:39:40 +0200

    test(e2e): fix i18n

--- app/test/e2e/i18n.test.ts
+++ app/test/e2e/i18n.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { setup, createPage, url } from '@nuxt/test-utils';
+import { setup, createPage } from '@nuxt/test-utils';
 import { mockNuxtImport } from '@nuxt/test-utils/runtime';
 
 mockNuxtImport('useI18n', () => () => useNuxtApp().$i18n);
@@ -7,25 +7,24 @@ mockNuxtImport('useI18n', () => () => useNuxtApp().$i18n);
 describe('i18n', async () => {
   await setup();
 
-  const { t: $t, setLocale } = useI18n();
-
   test('renders EN translation correctly in default route', async () => {
-    const page = await createPage();
-    await page.goto(url('/'));
+    const page = await createPage('/');
 
     const description = await page.textContent('p');
 
+    const { t: $t } = useI18n();
+
     expect(description).toBe($t('description'));
-  });
+  }, 10000);
 
   test('renders ES translation correctly in /es route', async () => {
-    setLocale('es');
-
-    const page = await createPage();
-    await page.goto(url('/es'));
+    const page = await createPage('/es');
 
     const description = await page.textContent('p');
 
+    const { t: $t, setLocale } = useI18n();
+    await setLocale('es');
+
     expect(description).toBe($t('description'));
-  });
+  }, 10000);
 });
